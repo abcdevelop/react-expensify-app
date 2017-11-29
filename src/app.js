95e406c3316866d985ff080d58173649fcdabe8e
abcdevelop <bruno.panaget@yahoo.fr>
2017-11-29 10:48:01 +0100

    Setup the Auth Reducer

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import {Provider} from 'react-redux';
 import AppRouter, {history} from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import {startSetExpenses} from './actions/expenses';
+import {login,logout} from './actions/auth';
 // import {addExpense} from './actions/expenses';
 // import {setTextFilter} from './actions/filters';
 // import getVisibleExpenses from './selectors/expenses';
@@ -36,6 +37,8 @@ ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 firebase.auth().onAuthStateChanged((user) => {
     if (user) {
         console.log('log in');
+        //console.log('uid',user.uid);
+        store.dispatch(login(user.uid));
         store.dispatch(startSetExpenses()).then(() => {
             renderApp();
             if(history.location.pathname === '/'){
@@ -44,6 +47,7 @@ firebase.auth().onAuthStateChanged((user) => {
         });
     } else {
         console.log('log out');
+        store.dispatch(logout());
         renderApp();
         history.push('/');
 
