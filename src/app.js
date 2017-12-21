c1705234e3e9937ef52cb5acae4cae51a6cc2ca5
abcdevelop <bruno.panaget@yahoo.fr>
2017-12-21 14:58:54 +0100

    Adding Loader

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@ import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 import 'react-dates/lib/css/_datepicker.css';
 import {firebase} from './firebase/firebase';
+import LoadingPage from './components/LoadingPage';
 // import './playground/promises';
 
 const store = configureStore();
@@ -31,8 +32,7 @@ const renderApp = () => {
     }
 };
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
-
+ReactDOM.render(<LoadingPage />, document.getElementById('app'));
 
 firebase.auth().onAuthStateChanged((user) => {
     if (user) {
@@ -50,9 +50,9 @@ firebase.auth().onAuthStateChanged((user) => {
         store.dispatch(logout());
         renderApp();
         history.push('/');
-
     }
 });
+
 
 // store.subscribe(() => {
 //     const state = store.getState();
